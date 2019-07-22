01c9079e7d96f31dd709563fdeb44bedd3e2ddd2
jollygrin <jollygrin@protonmail.com>
2019-07-22 23:21:34 +0200

    update

--- src/js/Kits.js
+++ src/js/Kits.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import airtable from 'airtable';
-import * as data from './now.json';
+// import * as data from '../now.json';
 
 export const getKits = async (doneBehavior) => {
 
     const Airtable = require('airtable');
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
-        apiKey: 'keyBj1QK7piMXmdoF'
+        apiKey: process.env.API_KEY
     });
-    const base = Airtable.base('appZ7HjeS9KkAUgyu');
+    const base = Airtable.base(process.env.BASE);
     const table = base('Kits');
 
     let acc = [];
